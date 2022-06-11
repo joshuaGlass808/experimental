dff78ef0c70f0109b70bc8f91954f8343b92d670
joshg <joshg@ca-it000101.corp.agiftinside.com>
2022-06-11 13:12:14 -0700

    Some random php string palindrome stuff.

---
+++ php/super-random/findLongestPalindrome.php
@@ -0,0 +1,40 @@
+<?php
+
+function findLargestPalindrome(string $str): string
+{
+    $length = strlen($str);
+    if ($length === 1) {
+        return $str;
+    }
+
+    $pal = '';
+    $pLen = 0;
+    $strArr = str_split($str);
+    foreach ($strArr as $i => $char) {
+        $tempStr = $char;
+        foreach ($strArr as $k => $sChar) {
+           if ($i === $k) {
+               if ($pLen === 0) {
+                   $pLen = 1;
+                   $pal = $char;
+               }
+               continue;
+           }
+           $tempStr .= $sChar;
+           if (strlen($tempStr) <= $pLen) {
+               continue;
+           }
+  
+           if ($tempStr === strrev($tempStr)) {
+               $pLen = strlen($tempStr);
+               $pal = $tempStr;
+           }
+        }
+        unset($strArr[$i]);
+    }
+
+    return $pal;
+}
+
+echo findLargestPalindrome('aab') . "\n";
+echo findLargestPalindrome('aabaabaaaaa') . "\n";
