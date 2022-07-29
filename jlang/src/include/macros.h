86226c9cdf5646bc536830faac5bade6bb475edb
joshuaGlass808 <joshua.lee.glass@gmail.com>
2022-07-29 02:06:00 +0000

    more updates to jlang compiler

---
+++ jlang/src/include/macros.h
@@ -0,0 +1,8 @@
+#ifndef JLANG_MACROS_H
+#define JLANG_MACROS_H
+#define MAX(a, b)\
+  a > b ? a : b
+
+  #define MIN(a, b)\
+  a < b ? a : b 
+#endif
--- jlang/src/main.c
+++ jlang/src/main.c
@@ -1,1 +1,12 @@
- 
+#include <stdio.h>
+#include "include/jlang.h"
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2) {
+        printf("Please specify file.\n\n");
+        return 1;
+    }
+
+    return 0;
+}
